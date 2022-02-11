9e88b20bb5d51928fcee6932ff075159995c340f
Pav Rao <pav0107@gmail.com>
2022-02-11 11:43:27 +0000

    initial styling

--- app/Http/Livewire/Comments.php
+++ app/Http/Livewire/Comments.php
@@ -6,6 +6,14 @@ use Livewire\Component;
 
 class Comments extends Component
 {
+    public $comments = [
+        [
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati expedita quae commodi qui eveniet maxime nulla, quidem nesciunt autem voluptate reiciendis voluptates accusantium ad. Cupiditate nemo reiciendis fugiat reprehenderit debitis.',
+            'created_at' => '3 min ago',
+            'creator' => 'Pav'
+        ]
+    ];
+
     public function render()
     {
         return view('livewire.comments');
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Livewire</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <livewire:styles />
 </head>
 <body>
-    @livewire('counter')
+
+    <livewire:comments />
     <livewire:scripts />
 </body>
 </html>
